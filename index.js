80017d7d7c32c7f2da823cac6779b01bbbdcac02
Virender <61196679+virender-7@users.noreply.github.com>
2024-01-18 16:34:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const http = require("http"); 
-//create a server object: 
-http 
-  .createServer(function (req, res) { 
-    res.write("<h1>Hello World!</h1>");  
-    //write a response to the client 
-     
-    res.end();  
-    //end the response 
-  }) 
-  .listen(8080);  
-//Server runs on localhost:8080 
+console.log('Hello, world!');
